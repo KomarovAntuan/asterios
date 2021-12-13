7715febc9bd56c5ed056af685892fdf80ae98f43
Komarov Anton <newdeathcinema@gmail.com>
2021-12-13 16:46:28 +0400

    Привязка к GHP

--- src/app/asterios/shared/header/shared.module.ts
+++ src/app/asterios/shared/header/shared.module.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FooterComponent } from "../footer/footer.component";
 import { HeaderComponent } from "./header.component";
 
 @NgModule({
     declarations: [
-        HeaderComponent
+        HeaderComponent,
+        FooterComponent
     ],
     exports: [
-        HeaderComponent
+        HeaderComponent,
+        FooterComponent
     ],
     imports: [
         CommonModule
